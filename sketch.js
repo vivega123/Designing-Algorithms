9450bcd9d8d782565d836180ff5e65d6f94ebd65
vivega123 <72975871+vivega123@users.noreply.github.com>
2020-10-26 16:42:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(800,400);
+  movingRect = createSprite(200, 100, 50, 50);
+  fixedRect = createSprite (400,200,40,40);
+}
+
+function draw() {
+
+  background(180); 
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+    if( movingRect.x - fixedRect.x < movingRect.width/2 + fixedRect.width/2
+      && fixedRect.x - movingRect.x < movingRect.width/2 + fixedRect.width/2
+      && fixedRect.y - movingRect.y < movingRect.height/2 + fixedRect.height/2
+      && movingRect.y - fixedRect.y < movingRect.height/2 + fixedRect.height/2
+      )
+    {
+      movingRect.shapeColor = "green" ;
+      fixedRect.shapeColor = "green" ;
+      
+    }
+
+    else 
+    {
+      movingRect.shapeColor = "red" ;
+      fixedRect.shapeColor = "red" ;
+    }
+
+     
+   
+  drawSprites();
+}
